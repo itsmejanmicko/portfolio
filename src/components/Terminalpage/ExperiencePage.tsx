602a2ce9ddb0debd628df71bc812e86f3c1ba49f
janmicko <mejia.johnmikkoi@gmail.com>
2024-12-03 06:36:05 -0800

    make the image static

--- src/components/Terminalpage/ExperiencePage.tsx
+++ src/components/Terminalpage/ExperiencePage.tsx
@@ -1,4 +1,6 @@
-
+import accessPoint from '../../../public/jmKGsL5s.jpg'
+import switchs from '../../../public/OZBthtS5.jpg'
+import cisco from '../../../public/QGJc_426.jpg'
 import TerminalNav from './TerminalNav'
 import ExperienceProps from '../../reducers/Experience'
 interface Project {
@@ -13,24 +15,25 @@ const projects: Project[] = [
     id: 1,
     title: "Access Point Deployment",
     description: "Designed and implemented a network using LAPAX access points, optimizing wireless connectivity for high-density environments. Configured advanced features like VLAN segmentation, seamless roaming, and security protocols to ensure reliable and secure Wi-Fi performance.",
-    imageUrl: "../../../public/jmKGsL5s.jpg",
+    imageUrl: accessPoint,
     tags: ["Cisco","Wi-Fi","Network" ]
   },
   {
     id: 2,
     title: "Switch Configuration",
     description: "Configured and deployed Cisco and Linksys switches to build scalable and efficient network infrastructures. Implemented VLANs, trunking, and port security to optimize traffic management and enhance network security. Performed troubleshooting and ensured seamless integration with existing network devices.",
-    imageUrl: "../../../public/OZBthtS5.jpg",
+    imageUrl: switchs,
     tags: ["Cisco", "Linksys", "Switches" ]
   },
   {
     id: 3,
     title: "Wireless Network Optimization",
     description: "Successfully integrated Cisco access points with network switches to provide robust and scalable wireless connectivity. Configured PoE (Power over Ethernet) ports on switches, ensured proper VLAN assignment, and optimized wireless settings for seamless connectivity and performance. Conducted testing and troubleshooting to guarantee reliability across the network.",
-    imageUrl: "../../../public/QGJc_426.jpg",
+    imageUrl: cisco,
     tags: ["Cisco","Switch", "Connectivity" ]
   },
 ]
+
 
 export default function ExperiencePage() {
   return (
@@ -54,19 +57,19 @@ export default function ExperiencePage() {
         </div>
 
         <h1 className="text-4xl font-bold text-center mb-12 mt-12">Work Highlights</h1>
-        <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="sm:w-screen mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {projects.map((project) => (
+            
             <div key={project.id} className=" bg-gray-900 text-gray-100 rounded-lg shadow-md overflow-hidden">
               <img
-                src={project.imageUrl}
-                alt={project.title}
-                width={600}
-                height={400}
-                className="w-full h-48 object-cover"
-              />
+              src={project.imageUrl}
+              alt={project.title}
+              className="w-full h-48 object-cover"
+            />
+
               <div className="p-6">
                 <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
-                <p className=" dark:text-white mb-4">{project.description}</p>
+                <p className=" mb-4">{project.description}</p>
                 <div className="flex flex-wrap gap-2">
                   {project.tags.map((tag, index) => (
                     <span
